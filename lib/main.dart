dda6506008d7335c29dc8263a7de5f8d59d88fa1
Atakan Oguzlar <atakanoguzlar@gmail.com>
2024-05-02 16:25:06 +0300

    Main class isim değişikliği

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bitirme/pages/auth_page.dart';
+import 'package:bitirme/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -8,11 +9,11 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(Klas());
+  runApp(ExpenseManagementApplication());
 }
 
-class Klas extends StatelessWidget {
-  const Klas({super.key});
+class ExpenseManagementApplication extends StatelessWidget {
+  const ExpenseManagementApplication({super.key});
 
   @override
   Widget build(BuildContext context) {
