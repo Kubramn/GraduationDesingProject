4ac401eee899bebdc424d4c877063a08094372d2
Atakan Oguzlar <atakanoguzlar@gmail.com>
2024-05-02 19:27:12 +0300

    "login_page" düzenlendi. "Log Out" butonu eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bitirme/pages/auth_page.dart';
-import 'package:bitirme/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -1,4 +1,5 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class FinancePage extends StatefulWidget {
@@ -8,11 +9,18 @@ class FinancePage extends StatefulWidget {
   State<FinancePage> createState() => _FinancePageState();
 }
 
+void logout() {
+  FirebaseAuth.instance.signOut();
+}
+
 class _FinancePageState extends State<FinancePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 187, 168, 255),
+      body: Column(
+        children: [ElevatedButton(onPressed: logout, child: Text("Log Out"))],
+      ),
       bottomNavigationBar: CurvedNavigationBar(
         color: Color.fromARGB(135, 79, 46, 150),
         backgroundColor: Color.fromARGB(255, 187, 168, 255),
